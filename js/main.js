2af57b701da33d94277c8f35b7f54c3069b4d32c
Sabrina Cinzer <sabrinacinzer@gmail.com>
2022-07-04 13:06:41 -0300

    Updated code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var arr = new Array();
+    $("select[multiple]").change(function() {
+        $(this).find("option:selected")
+        if ($(this).find("option:selected").length > 3) {
+            $(this).find("option").removeAttr("selected");
+            $(this).val(arr);
+        }
+        else {
+            arr = new Array();
+            $(this).find("option:selected").each(function(index, item) {
+                arr.push($(item).val());
+            });
+        }
+    });
+});
